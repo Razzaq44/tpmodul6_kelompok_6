35775b61ab8d332acb3145736eb4c8ae859e6670
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-04-01 17:11:02 +0700

    menambahkan class DataMahasiswa1302204128 dan class Mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Minggu6
+{
+    public class Mahasiswa
+    {
+        public NamaMahasiswa nama { get; set; }
+        public int nim { get; set; }
+        public string fakultas { get; set; }
+
+        public Mahasiswa() { }
+    }
+
+    public class NamaMahasiswa
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+
+        public NamaMahasiswa() { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace Minggu6
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Mahasiswa objHasilBaca = DataMahasiswa.ReadJSON();
+            Console.WriteLine("nama " + objHasilBaca.nama.depan + " " + objHasilBaca.nama.belakang + " dengan nim " + objHasilBaca.nim + " dari fakultas " + objHasilBaca.fakultas);
+        }
+    }
+}
